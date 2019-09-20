155b1c2331914373ad4d085cb0ef4dd59e0d4c35
Jacky Jiawei Mao <jmao8307@uni.sydney.edu.au>
2019-09-20 10:43:05 +1000

    Merge
    Temperorary

---
+++ Assets/Script/resolutionManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class resolutionManager : MonoBehaviour
+{
+    public int width;
+    public int height;
+
+    public void SetWidth(int w) { this.width = w; }
+    public void SetHeight(int h) { this.height = h; }
+
+    public void Set_res() { Screen.SetResolution(width, height, false); }
+
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+        
+    // }
+}
