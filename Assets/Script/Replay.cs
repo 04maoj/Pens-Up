24d87b18a5f3799e8bd51b3246f8ad0a52f6c8a1
Hako <aaron1819855527@gmail.com>
2019-09-26 00:01:38 +1000

    ->Select Student->Select Letter

--- Assets/Script/Replay.cs
+++ Assets/Script/Replay.cs
@@ -18,11 +18,12 @@ public class Replay : MonoBehaviour
     Vector3 currentPosition;
     int index = -1;
     // X-axis offset of the character
-    public float offSetX = 140f;
+    public float offSetX;
     // Y-axis offset of the character
-    public float offSetY = -47.65f;
+    public float offSetY;
     // Z-axis offset of the drawing space
     public float offSetZ = -1f;
+    public float scale = 1;
     
     // Start is called before the first frame update
     void Start()
@@ -36,12 +37,12 @@ public class Replay : MonoBehaviour
     // Update is called once per frame
     void OnClick()
     {
-        Debug.Log("'Replay' Pressed.");
+        // Debug.Log("'Replay' Pressed.");
         // drawManager.setCharacter(character);
         Debug.Log("Replay char: " + character);
         drawManager.setCharacter(character);
         coordinates = drawManager.GetStrokes(character);
-        Debug.Log("Got Strokes");
+        // Debug.Log("Got Strokes");
         // drawManager.DrawBot(coordinates);
         StartCoroutine(WaitAndPaint());
         // DrawBot(coordinates);
@@ -49,42 +50,42 @@ public class Replay : MonoBehaviour
         
     }
 
-    public void DrawBot(List<List<Tuple<float, float>>> coordinates){
-        int count = 0;
-        foreach (List<Tuple<float, float>> eachStroke in coordinates){
-            clone = (GameObject)Instantiate(target,target.transform.position,Quaternion.identity);
-            lineRe = clone.GetComponent<LineRenderer>();
-            lineRe.startColor = Color.red;
-            lineRe.endColor = Color.blue;
-            lineRe.startWidth = 2f;
-            lineRe.endWidth = 2f;
-            count = eachStroke.Count;
-            // vectorList = new List<Vector3>();
-            // List<Vector3> stroke = new List<Vector3>();
-            stroke = new List<Vector3>();
-            foreach (Tuple<float, float> point in eachStroke){
-                // Vector3 pointCoordinate = new Vector3(point.Item1, point.Item2, clone.transform.position.z);
-                Vector3 pointCoordinate = new Vector3(point.Item1 + offSetX, point.Item2 + offSetY, offSetZ);
-                
-                stroke.Add(pointCoordinate);
-            }
-            lineRe.positionCount = count;
+    // public void DrawBot(List<List<Tuple<float, float>>> coordinates){
+    //     int count = 0;
+    //     foreach (List<Tuple<float, float>> eachStroke in coordinates){
+    //         clone = (GameObject)Instantiate(target,target.transform.position,Quaternion.identity);
+    //         lineRe = clone.GetComponent<LineRenderer>();
+    //         lineRe.startColor = Color.red;
+    //         lineRe.endColor = Color.blue;
+    //         lineRe.startWidth = 2f;
+    //         lineRe.endWidth = 2f;
+    //         count = eachStroke.Count;
+    //         // vectorList = new List<Vector3>();
+    //         // List<Vector3> stroke = new List<Vector3>();
+    //         stroke = new List<Vector3>();
+    //         foreach (Tuple<float, float> point in eachStroke){
+    //             // Vector3 pointCoordinate = new Vector3(point.Item1, point.Item2, clone.transform.position.z);
+    //             Vector3 pointCoordinate = new Vector3(point.Item1 + offSetX, point.Item2 + offSetY, offSetZ);
+    //             Debug.Log("XYZ: " + pointCoordinate);
+    //             stroke.Add(pointCoordinate);
+    //         }
+    //         lineRe.positionCount = count;
 
-            Debug.Log("lineRe_Count: " + lineRe.positionCount);
-            currentPosition = new Vector3();
+    //         // Debug.Log("lineRe_Count: " + lineRe.positionCount);
+    //         currentPosition = new Vector3();
 
-            for (int i = 0; i < lineRe.positionCount; i++){
-                index = i;
-                currentPosition = stroke[i];
-                Debug.Log("Index: " + index + " Pos: " + currentPosition);
-                StartCoroutine(WaitAndPaint());
-                // lineRe.SetPosition(i, currentPosition);
+    //         for (int i = 0; i < lineRe.positionCount; i++){
+    //             index = i;
+    //             currentPosition = stroke[i];
+    //             // Debug.Log("Index: " + index + " Pos: " + currentPosition);
+    //             StartCoroutine(WaitAndPaint());
+    //             // lineRe.SetPosition(i, currentPosition);
                 
-                // StartCoroutine(WaitAndDraw(lineRe, i, stroke[i]));
+    //             // StartCoroutine(WaitAndDraw(lineRe, i, stroke[i]));
                 
-            }
-        }
-    }
+    //         }
+    //     }
+    // }
 
     IEnumerator WaitAndPaint(){
     
@@ -110,8 +111,8 @@ public class Replay : MonoBehaviour
             stroke = new List<Vector3>();
             foreach (Tuple<float, float> point in eachStroke){
                 // Vector3 pointCoordinate = new Vector3(point.Item1, point.Item2, clone.transform.position.z);
-                Vector3 pointCoordinate = new Vector3(point.Item1 + offSetX, point.Item2 + offSetY, offSetZ);
-                
+                Vector3 pointCoordinate = new Vector3((point.Item1 + offSetX) / scale, (point.Item2 + offSetY) / scale, offSetZ);
+                Debug.Log("XYZ: " + pointCoordinate);
                 stroke.Add(pointCoordinate);
             }
             lineRe.positionCount = count;
