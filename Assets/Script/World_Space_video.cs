9b881ee1c22fd3687431c5bb81664efce66befba
Jacky Jiawei Mao <jmao8307@uni.sydney.edu.au>
2019-09-28 17:31:29 +1000

    Finished Vido tutorial
    FInished the videio tutorial section

---
+++ Assets/Script/World_Space_video.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class World_Space_video : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public VideoPlayer myVideo;
+    public GameObject endButton;
+    public GameObject other_UI;
+    public void Awake()
+    {   
+        myVideo.loopPointReached += CheckOver;
+        endButton.SetActive(false);
+        other_UI.SetActive(false);
+    }
+    public void PlayPause()
+    {
+        if (myVideo.isPlaying)
+        {
+            myVideo.Pause();
+        }
+        else
+        {
+            myVideo.Play();
+        }
+    }
+    void CheckOver(VideoPlayer vp) {
+        endButton.SetActive(true);
+    }
+    public void End()
+    {
+        other_UI.SetActive(true);
+        gameObject.SetActive(false);
+    }
+
+}
