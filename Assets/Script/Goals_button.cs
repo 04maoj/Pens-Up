f8f62c216d2fd6f4813ea6b13130564f178eb7d0
zenithz <zenithz@Flynns-Air.hub>
2019-10-02 01:16:09 +1000

    S13 - Err UnassignedReferenceException: The variable m_Content of ScrollRect has not been assignd.

--- Assets/Script/Goals_button.cs
+++ Assets/Script/Goals_button.cs
@@ -3,30 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
-public class Register : MonoBehaviour
+public class Goals_button : MonoBehaviour
 {   
-    //0 for teacher and 1 for student
-    int status = 0;
-    [SerializeField] public Dropdown drop;
-    [SerializeField] public GameObject password;
-    [SerializeField] public GameObject user_name;
+    [SerializeField] public GameObject InputField1;
+    [SerializeField] public GameObject InputField2;
+    [SerializeField] public GameObject InputField3;
+    [SerializeField] public GameObject InputField4_day;
+    [SerializeField] public GameObject InputField4_min;
     [SerializeField] public GameObject success_text;
     public void setAttemp()
     {
-        status = drop.value;
-        string goal1 = inputfield1.GetComponent <InputField>().text;
-        string goal2 = inputfield2.GetComponent<InputField>().text;
-        string goal3 = inputfield3.GetComponent <InputField>().text;
-        string goal4a = inputfield4_min.GetComponent<InputField>().text;
-        string goal4b = inputfield4_day.GetComponent <InputField>().text;
+        string goal1 = InputField1.GetComponent <InputField>().text;
+        string goal2 = InputField2.GetComponent<InputField>().text;
+        string goal3 = InputField3.GetComponent <InputField>().text;
+        string goal4a = InputField4_min.GetComponent<InputField>().text;
+        string goal4b = InputField4_day.GetComponent <InputField>().text;
+        
         string path = "";
-        if (status == 0)
-        {
-            path = "Assets/Local_DataBase/Students/Student_Goal.txt";
-        } 
-        else {
-            path = "Assets/Local_DataBase/Students/Student_Goal.txt";
-        }
+        path = "Assets/Local_DataBase/Students/Student_Goal.txt";
 
         using (StreamWriter sw = File.AppendText(path))
         {
