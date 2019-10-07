7101128f3df5d860bf273665685e111944fb8e1b
minz0914 <minz0914@outlook.com>
2019-10-07 17:00:01 +1100

    Add the date of birth function for teacher profile

---
+++ Assets/Script/Setdob.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Setdob : MonoBehaviour
+{
+  public InputField year;
+  public InputField month;
+  public InputField day;
+  public Text dob;
+  public void setget(){
+    dob.text=day.text + "-" + month.text + "-" + year.text;
+    }
+}
