bd7ae0faa0a2df5a66b6893584e64adc6add920d
Jacky Jiawei Mao <jmao8307@uni.sydney.edu.au>
2019-10-07 16:30:47 +1100

    Added the ranking functions
    Ranking system is added

--- Assets/Script/Ranking_Init.cs
+++ Assets/Script/Ranking_Init.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using UnityEngine.UI;
 using System;
 public class Ranking_Init : MonoBehaviour
 {
@@ -12,12 +13,18 @@ public class Ranking_Init : MonoBehaviour
     public GameObject spawner;
     void Start()
     {
+        ranks = new List<Tuple<double, string>>();
         t_n = FindObjectOfType<User_Info>().Get_Teacher();
         string path = "Assets/Local_DataBase/Teachers/" + t_n + "/Student_List.txt";
         StreamReader myReader = new StreamReader(path);
         while(!myReader.EndOfStream) {
             string student = myReader.ReadLine();
             string path_1 = "Assets/Local_DataBase/Students/" + student + "/Total_score_list";
+            if(!File.Exists(path_1))
+            {
+                ranks.Add(new Tuple<double, string>(0, student));
+                continue;
+            }
             Dictionary<string, int> score_list = new Dictionary<string, int>();
             StreamReader mysecondReader = new StreamReader(path_1);
             while(!mysecondReader.EndOfStream)
@@ -34,15 +41,18 @@ public class Ranking_Init : MonoBehaviour
             }
             double total_score = 0;
             foreach (var kvp in score_list)
-                total_score = score_list[kvp.Key];
+                total_score += score_list[kvp.Key];
             total_score /= number_course;
             ranks.Add(new Tuple<double, string>(total_score, student));
         }
         ranks.Sort();
+        ranks.Reverse();
         for(int i = 0; i < ranks.Count; i++)
         {
-            var current_spawned = (spawner, transform.position, transform.rotation);
-            //current_spawne;
+            var current_spawned = Instantiate(spawner, transform.position, Quaternion.identity);
+            current_spawned.gameObject.transform.SetParent(Content.transform);
+            current_spawned.transform.localScale = new Vector3(1, 1, 1);
+            current_spawned.transform.GetChild(0).GetComponent<Text>().text = ranks[i].Item1 + " " + ranks[i].Item2;
         }
     }
 
