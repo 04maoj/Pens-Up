d26e118eae7f06e69310dad93ca7ebd7335f7b5b
minz0914 <minz0914@outlook.com>
2019-10-08 16:54:00 +1100

    Add dynamic list page

---
+++ Assets/Script/list.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class list : MonoBehaviour
+{
+    // Start is called before the first frame update
+	public GameObject spawner;
+	public GameObject Content;
+    void Start()
+    {
+		for (int i=0; i<3; i++) {
+        var current_spawned = Instantiate(spawner, transform.position, Quaternion.identity);
+        current_spawned.gameObject.transform.SetParent(Content.transform);
+        current_spawned.transform.localScale = new Vector3(1, 1, 1);
+        current_spawned.transform.GetChild(0).GetComponent<Text>().text = "Hey";
+				current_spawned.transform.position = new Vector3(550+i*800,1000,1000);
+	}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
