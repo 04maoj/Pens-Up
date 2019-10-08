40e85f0a1e7f5e1889ffd9ee3a8b38d1bb864ca6
zenithz <zenithz@Flynns-MacBook-Air.local>
2019-10-09 03:37:41 +1100

    goals v3, retriveGoals.cs BUG. goals ui manager bug.

---
+++ Assets/Images/C# switch statement _ Microsoft Docs_files/t.js
@@ -0,0 +1,1 @@
+document.cookie="MSFPC=GUID=9f5de76331c542609242747612c5fd99&HASH=9f5d&LV=201811&V=4&LU=1541744848471;expires=Wed, 07 Oct 2020 16:07:56 GMT;path=/";if(awa.ix){awa.ix.set({"mc1":"9f5de76331c542609242747612c5fd99"})};if(awa.firstEventDone){awa.firstEventDone()};
--- Assets/Script/Goals_button.cs
+++ Assets/Script/Goals_button.cs
@@ -5,34 +5,41 @@ using UnityEngine.UI;
 using System.IO;
 public class Goals_button : MonoBehaviour
 {   
-    [SerializeField] public GameObject InputField1;
-    [SerializeField] public GameObject InputField2;
-    [SerializeField] public GameObject InputField3;
-    [SerializeField] public GameObject InputField4_day;
-    [SerializeField] public GameObject InputField4_min;
-    [SerializeField] public GameObject success_text;
+    // [SerializeField] public GameObject InputField1;
+    [SerializeField] public Dropdown drop;
+    int status = 0;
+
     public void setAttemp()
     {
-        string goal1 = InputField1.GetComponent <InputField>().text;
-        string goal2 = InputField2.GetComponent<InputField>().text;
-        string goal3 = InputField3.GetComponent <InputField>().text;
-        string goal4a = InputField4_min.GetComponent<InputField>().text;
-        string goal4b = InputField4_day.GetComponent <InputField>().text;
-        
-        string path = "";
-        path = "Assets/Local_DataBase/Students/Student_Goal.txt";
+        status = drop.value;
+        string user_name = "Handsome";
+        string path = "Assets/Local_DataBase/Students/" + user_name + "goals.txt";
+
 
         using (StreamWriter sw = File.AppendText(path))
         {
-            sw.WriteLine(goal1 + " " + goal2 + " " + goal3 + " " + goal4a + " " + goal4b);
-            StartCoroutine("Back_To_Home");
+            switch (status)
+            {
+                case 0: 
+                    sw.WriteLine("BASIC"); 
+                    break;
+                case 1:
+                    sw.WriteLine("INTER");
+                    break;
+                case 2: 
+                    sw.WriteLine("EXPERT"); 
+                    break;
+                default: 
+                    StartCoroutine("Back_To_Home");
+                    break;
+            }
         }
     }
+
     public IEnumerator Back_To_Home()
     {
-        success_text.SetActive(true);
         yield return new WaitForSeconds(2);
         SceneLoader q = FindObjectOfType<SceneLoader>();
-        q.LoadScence(11);
+        q.LoadScence(7);
     }
 }
