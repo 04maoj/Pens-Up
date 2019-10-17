7ab50c131fb64e9cdbc3149f404b6f006bc782c0
minz0914 <minz0914@outlook.com>
2019-10-17 15:38:16 +1100

    interface change

--- Assets/Script/studentdetails.cs
+++ Assets/Script/studentdetails.cs
@@ -8,7 +8,6 @@ public class studentdetails : MonoBehaviour
 {
     // Start is called before the first frame update
     public string u_name;
-    public Text dob;
     public string birthday;
     private HashSet<string> complete_set;
     private HashSet<string> complete_set2;
@@ -17,20 +16,7 @@ public class studentdetails : MonoBehaviour
     void Start()
     {
       u_name = staticname.i_name;
-      string teacher_path = "Assets/Local_DataBase/Students/" + u_name  + "/config.txt";
-      //  Debug.Log(u_name+" " + teacher_path);
-        complete_set = new HashSet<string>();
-        if(File.Exists(teacher_path)) {
-            // Debug.Log("Yes It exist");
-            string[] lines = File.ReadAllLines(teacher_path);
-            foreach (string line in lines) {
-              complete_set.Add(line);
-              // Debug.Log(line);
-              birthday = line;
-            }
-        }
-        Debug.Log(birthday);
-        dob.text=birthday;
+      
         string course_path = "Assets/Local_DataBase/Students/" + u_name  + "/Complted_course";
         Debug.Log(u_name+" " + course_path);
         complete_set2 = new HashSet<string>();
